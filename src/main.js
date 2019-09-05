de447115b463cb336152517d4b8d58056cdaaa95
David Abraham <doodle20091@gmail.com>
2019-09-05 15:11:57 -0400

    Service Worker
    Added service worker. Test 1 to see if registers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import App from './components/App.svelte'
 
+window.addEventListener('load', () => {
+	registerSW()
+});
+
+async function registerSW() {
+	if('serviceWorker' in navigator){
+	  navigator.serviceWorker.register('sw.js')
+	  .then((reg) => console.log('service worker registered', reg))
+	  .catch((err) => console.log('service worker not registered', err))
+	}
+}
 const app = new App({
 	target: document.body,
 })
